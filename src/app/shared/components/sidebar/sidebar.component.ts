4213ebd8d90af3d18f3b4d693c32ca2f729363ad
Carolina Bolivar <cdelterror123@gmail.com>
2024-10-17 15:42:58 -0500

    cap-99

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
+
 })
 export class SidebarComponent {
+
 
   constructor ( private gifsService: GifsService) {
 
@@ -17,5 +19,8 @@ export class SidebarComponent {
     return this.gifsService.tagsHistory;
   }
 
+  searchTag (tag: string){
+    this.gifsService.searchTag(tag);
+  }
 
 }
